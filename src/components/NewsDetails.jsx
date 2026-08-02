3570bfdf59fa4795da371c76292cfbf58f5803f4
Andy Acosta-Monterrosa <andyac09@hotmail.com>
2026-08-02 16:53:43 -0500

    fix: Update article 7 to include all verbatim paragraphs without summarization

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -62,7 +62,7 @@ const NewsDetails = () => {
   const reference = t(`${newsKey}.reference`);
   const keywords = t(`${newsKey}.keywords`, { returnObjects: true });
   
-  const paragraphs = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14', 'p15'];
+  const paragraphs = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20'];
   const currentUrl = window.location.href;
 
   const shareText = encodeURIComponent(title);
