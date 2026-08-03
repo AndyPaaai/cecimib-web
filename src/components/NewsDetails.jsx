c538e8e0c658b44c9307f3f676948f1bb95d0b5f
Andy Acosta-Monterrosa <andyac09@hotmail.com>
2026-08-02 21:41:03 -0500

    feat: Add article 8 (intestinal failure systematic review)

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -110,7 +110,7 @@ const NewsDetails = () => {
                   src={images[id]} 
                   alt={title} 
                   className="news-floating-image" 
-                  style={['1-6-2026', '2-4-2026', '2-6-2026', '2-7-2026'].includes(id) ? { width: '82%', maxWidth: '360px' } : {}}
+                  style={['1-6-2026', '2-4-2026', '2-6-2026', '2-7-2026', '2-8-2026'].includes(id) ? { width: '82%', maxWidth: '360px' } : {}}
                 />
               ) : (
                 <div className="news-floating-image placeholder-img"></div>
--- src/data/newsData.js
+++ src/data/newsData.js
@@ -8,11 +8,16 @@ import columna4Img from '../assets/columna4.png';
 import columna5Img from '../assets/columna5.png';
 import columna6Img from '../assets/columna6.png';
 import columna7Img from '../assets/columna7.png';
+import columna8Img from '../assets/columna8.png';
 
 // Single source of truth for news articles (images and order).
 // The text content (title, excerpt, date, etc) is driven by the translation files (es.json / en.json) 
 // using the 'id' as the key.
 export const newsData = [
+    {
+        id: '2-8-2026',
+        image: columna8Img,
+    },
     {
         id: '2-7-2026',
         image: columna7Img,
